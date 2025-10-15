872152e71baa58b037f764952db7781b17485537
Lama <lamamamoun90@gmail.com>
2025-10-15 13:43:45 +0200

    Har lagt till importen

--- app/(tabs)/chores/edit/[id].tsx
+++ app/(tabs)/chores/edit/[id].tsx
@@ -1,6 +1,6 @@
-import { View, Text } from 'react-native';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { Text, View } from 'react-native';
 import { Button } from 'react-native-paper';
-import { useRouter, useLocalSearchParams } from 'expo-router';
 
 export default function EditChoreScreen() {
   const router = useRouter();
